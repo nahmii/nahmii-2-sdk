8dbab633f45e27a22238ea793fc97d5d994e41a2
Hein <heindauven@gmail.com>
2021-12-14 23:41:48 +0100

    Add hex address formatting functions.

---
+++ src/string-format.ts
@@ -0,0 +1,84 @@
+/* Imports: External */
+import { BigNumber } from 'ethers'
+
+/**
+ * Removes "0x" from start of a string if it exists.
+ *
+ * @param str String to modify.
+ * @returns the string without "0x".
+ */
+export const remove0x = (str: string): string => {
+  if (str === undefined) {
+    return str
+  }
+  return str.startsWith('0x') ? str.slice(2) : str
+}
+
+/**
+ * Adds "0x" to the start of a string if necessary.
+ *
+ * @param str String to modify.
+ * @returns the string with "0x".
+ */
+export const add0x = (str: string): string => {
+  if (str === undefined) {
+    return str
+  }
+  return str.startsWith('0x') ? str : '0x' + str
+}
+
+/**
+ * Casts a hex string to a buffer.
+ *
+ * @param inp Input to cast to a buffer.
+ * @return Input cast as a buffer.
+ */
+/*export const fromHexString = (inp: Buffer | string): Buffer => {
+  if (typeof inp === 'string' && inp.startsWith('0x')) {
+    return Buffer.from(inp.slice(2), 'hex')
+  }
+
+  return Buffer.from(inp)Converts aBigNumberish to a HexString, with no unneces
+}*/
+
+/**
+ * Casts an input to a hex string.
+ *
+ * @param inp Input to cast to a hex string.
+ * @return Input cast as a hex string.
+ */
+//export const toHexString = (inp: Buffer | string |  number | null): string => {
+export const toHexString = (inp: number | BigNumber): string => {
+  if (typeof inp === 'number') {
+    return BigNumber.from(inp).toHexString()
+  } else {
+    throw "not implemented"
+    // return '0x' + fromHexString(inp).toString('hex')
+  }
+}
+
+export const toRpcHexString = (n: number | BigNumber): string => {
+  let num
+  if (typeof n === 'number') {
+    num = '0x' + n.toString(16)
+  } else {
+    num = n.toHexString()
+  }
+
+  if (num === '0x0') {
+    return num
+  } else {
+    return num.replace(/^0x0/, '0x')
+  }
+}
+
+export const padHexString = (str: string, length: number): string => {
+  if (str.length === 2 + length * 2) {
+    return str
+  } else {
+    return '0x' + str.slice(2).padStart(length * 2, '0')
+  }
+}
+
+export const encodeHex = (val: any, len: number) =>
+  remove0x(BigNumber.from(val).toHexString()).padStart(len, '0')
