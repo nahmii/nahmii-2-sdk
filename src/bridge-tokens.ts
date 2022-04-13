de9c5cc3a7b0b24cba26ff3d96b32e5181b7a852
Jens Ivar Jordre <jensivar@nahmii.io>
2022-04-13 11:32:04 +0200

    Add functions to deposit ETH and ERC20 to account other than sender

--- src/bridge-tokens.ts
+++ src/bridge-tokens.ts
@@ -10,7 +10,7 @@ export { relayL2ToL1Messages }
  * Deposit Ether.
  *
  * @param bridgeAddress L1 bridge address.
- * @param depositAmount The amount to deposit in wei.
+ * @param depositAmount Amount to deposit in wei.
  * @param l1Provider L1 provider.
  * @param signer L1 transaction signer.
  * @returns Returns the transaction response containing metadata for the ETH deposit transaction.
@@ -23,12 +23,10 @@ export const depositETH = async (
 ): Promise<ethers.providers.TransactionResponse> => {
   const L1StandardBridgeInterface = new ethers.utils.Interface(L1StandardBridgeABI)
   const contract = new ethers.Contract(bridgeAddress, L1StandardBridgeInterface, l1Provider)
-  const transactionResponse = await contract.connect(signer).depositETH(DEFAULT_GAS_L2, '0xFFFF', {
+  return contract.connect(signer).depositETH(DEFAULT_GAS_L2, '0xFFFF', {
     value: depositAmount,
     gasLimit: ETH_GAS_LIMIT_L1,
   })
-
-  return transactionResponse
 }
 
 /**
@@ -37,7 +35,7 @@ export const depositETH = async (
  * @param l1TokenAddress L1 address of the token to deposit.
  * @param l2TokenAddress L2 address of the mapped equivalent of the L1 token.
  * @param bridgeAddress L1 bridge address.
- * @param depositAmount The amount to deposit in wei.
+ * @param depositAmount Amount to deposit in wei.
  * @param l1Provider L1 provider.
  * @param signer L1 transaction signer.
  * @returns Returns the transaction response containing metadata for the ETH deposit transaction.
@@ -52,11 +50,60 @@ export const depositERC20 = async (
 ): Promise<ethers.providers.TransactionResponse> => {
   const L1StandardBridgeInterface = new ethers.utils.Interface(L1StandardBridgeABI)
   const contract = new ethers.Contract(bridgeAddress, L1StandardBridgeInterface, l1Provider)
-  const transactionResponse = await contract
-    .connect(signer)
-    .depositERC20(l1TokenAddress, l2TokenAddress, depositAmount, DEFAULT_GAS_L2, '0x')
+  return contract.connect(signer).depositERC20(l1TokenAddress, l2TokenAddress, depositAmount, DEFAULT_GAS_L2, '0x')
+}
 
-  return transactionResponse
+/**
+ * Deposit ERC20 token to target account. The L1 token requires a mapped L2 token to be deployed.
+ *
+ * @param bridgeAddress L1 bridge address.
+ * @param accountAddress Address of the target account on L2.
+ * @param depositAmount Amount to deposit in wei.
+ * @param l1Provider L1 provider.
+ * @param signer L1 transaction signer.
+ * @returns Returns the transaction response containing metadata for the ETH deposit transaction.
+ */
+export const depositETHTo = async (
+  bridgeAddress: string,
+  accountAddress: string,
+  depositAmount: BigNumber,
+  l1Provider: ethers.providers.JsonRpcProvider,
+  signer: ethers.Signer
+): Promise<ethers.providers.TransactionResponse> => {
+  const L1StandardBridgeInterface = new ethers.utils.Interface(L1StandardBridgeABI)
+  const contract = new ethers.Contract(bridgeAddress, L1StandardBridgeInterface, l1Provider)
+  return contract.connect(signer).depositETHTo(accountAddress, DEFAULT_GAS_L2, '0x', {
+    value: depositAmount,
+    gasLimit: ETH_GAS_LIMIT_L1,
+  })
+}
+
+/**
+ * Deposit ERC20 token to target account. The L1 token requires a mapped L2 token to be deployed.
+ *
+ * @param l1TokenAddress L1 address of the token to deposit.
+ * @param l2TokenAddress L2 address of the mapped equivalent of the L1 token.
+ * @param bridgeAddress L1 bridge address.
+ * @param accountAddress Address of the target account on L2.
+ * @param depositAmount Amount to deposit in wei.
+ * @param l1Provider L1 provider.
+ * @param signer L1 transaction signer.
+ * @returns Returns the transaction response containing metadata for the ETH deposit transaction.
+ */
+export const depositERC20To = async (
+  l1TokenAddress: string,
+  l2TokenAddress: string,
+  bridgeAddress: string,
+  accountAddress: string,
+  depositAmount: BigNumber,
+  l1Provider: ethers.providers.JsonRpcProvider,
+  signer: ethers.Signer
+): Promise<ethers.providers.TransactionResponse> => {
+  const L1StandardBridgeInterface = new ethers.utils.Interface(L1StandardBridgeABI)
+  const contract = new ethers.Contract(bridgeAddress, L1StandardBridgeInterface, l1Provider)
+  return contract
+    .connect(signer)
+    .depositERC20To(l1TokenAddress, l2TokenAddress, accountAddress, depositAmount, DEFAULT_GAS_L2, '0x')
 }
 
 /**
@@ -76,9 +123,7 @@ export const initiateWithdrawal = async (
 ): Promise<ethers.providers.TransactionResponse> => {
   const L2StandardBridgeInterface = new ethers.utils.Interface(L2StandardBridgeABI)
   const contract = new ethers.Contract(predeploys.NVM_L2StandardBridge, L2StandardBridgeInterface, l2Provider)
-  const transactionResponse = await contract.connect(signer).withdraw(l2TokenAddress, withdrawAmount, 0, '0x')
-
-  return transactionResponse
+  return contract.connect(signer).withdraw(l2TokenAddress, withdrawAmount, 0, '0x')
 }
 
 export const finalizeWithdrawal = relayL2ToL1Messages
